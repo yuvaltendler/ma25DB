52dd38aacf9bd5decfcbd0e7b38ae5e802176c64
Yuval t <yuval.tendler@gmail.com>
2022-01-19 19:17:43 +0200

    implement TableManager

--- src/main/java/FileManager/TableManager.java
+++ src/main/java/FileManager/TableManager.java
@@ -12,18 +12,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class FileManager {
-    private static FileManager instance;
+public class TableManager {
+    private static TableManager instance;
 
     private final int MUX_RECORD_IN_FILE;
     private @Getter @Setter HashMap<String, ArrayList<String>> tableNameToPaths;
 
     public JSONObject getTableByRow(String tableName, int row){
+        return this.getTableByIndex(tableName, row/MUX_RECORD_IN_FILE);
+    }
+
+    public ArrayList<JSONObject> getTable(String tableName){
+        ArrayList<JSONObject> res = new ArrayList<>();
+        for(int i = 0; i < this.tableNameToPaths.get(tableName).size(); i++){
+            res.add(this.getTableByIndex(tableName, i));
+        }
+        return res;
+    }
+
+    private JSONObject getTableByIndex(String tableName, int inx){
         JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader(this.tableNameToPaths.get(tableName).get(row/MUX_RECORD_IN_FILE)))
-        {
-            Object obj = jsonParser.parse(reader);
+        try {
+            Object obj  = jsonParser.parse(new FileReader(String.valueOf(new FileReader(this.tableNameToPaths.get(tableName).get(inx)))));
             return (JSONObject) obj;
+
 
         } catch (IOException | ParseException e) {
             e.printStackTrace();
@@ -31,18 +43,18 @@ public class FileManager {
         return null;
     }
 
-    private FileManager(int mux_record_in_file){
+    private TableManager(int mux_record_in_file){
         MUX_RECORD_IN_FILE = mux_record_in_file;
     }
 
-    public static FileManager getInstance(int mux_record_in_file){
+    public static TableManager getInstance(int mux_record_in_file){
         if(instance == null){
-            instance = new FileManager(mux_record_in_file);
+            instance = new TableManager(mux_record_in_file);
         }
         return instance;
     }
 
-    public static FileManager getInstance(){
+    public static TableManager getInstance(){
         if(instance != null){
             return instance;
         }
