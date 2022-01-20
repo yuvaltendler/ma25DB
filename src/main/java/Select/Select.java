7ce1459effb795fb13cd5f4dadb2b990b4f0772e
Yuval t <yuval.tendler@gmail.com>
2022-01-20 18:07:33 +0200

    create select class

---
+++ src/main/java/Select/Select.java
@@ -0,0 +1,14 @@
+package Select;
+
+import Transform.ParseTable;
+import org.json.simple.JSONObject;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class Select {
+//    JSONObject select(JSONObject table, Condition condition){
+//        Stream<Collection> selectStream = Stream.of(ParseTable.getData(table).values()).filter(record -> condition.isRecordPassCondition((JSONObject) record));
+//        return selectStream.toArray();
+//    }
+}
